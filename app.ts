a08938dddce95da414e57bdaa2efe1e061bac84c
zakapaiglaza <mister.alex.gromv@gmail.com>
2024-07-03 14:37:43 +0300

    fix fix

--- app.ts
+++ app.ts
@@ -12,7 +12,7 @@ import { WorkerRoutes } from './src/WORKERS/routWorker';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 
 
